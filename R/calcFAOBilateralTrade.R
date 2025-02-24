76f6ece36c8db79f6adc6d5cae5ebae88bd758b1
caviddhen <david.chen@pik.potsdam.de>
2025-02-24 18:38:06 +0100

    bugfix trade mapping

--- R/calcFAOBilateralTrade.R
+++ R/calcFAOBilateralTrade.R
@@ -123,7 +123,7 @@ calcFAOBilateralTrade <- function(output = "value", products = "kcr", prodAgg = 
     # aggregate to get a preliminary cif/fob ratio
     out[is.na(out)] <- 0
     mapping <- toolGetMapping("FAO_trade_k_mapping.csv", type = "sectoral", where = "mrfaocore")
-    mapping <- mapping[which(mapping$FAOoriginalItem_fromWebsite %in% getNames(out)),c("post2010_FAOoriginalItem_fromWebsite", "k")]
+    mapping <- mapping[which(mapping$post2010_FAOoriginalItem_fromWebsite %in% getNames(out)),c("post2010_FAOoriginalItem_fromWebsite", "k")]
     mapping <- mapping[which(mapping$k != ""), ]
     out <- toolAggregate(out, rel = mapping, from = "post2010_FAOoriginalItem_fromWebsite",
                          to = "k", partrel = TRUE, dim = 3.1)
